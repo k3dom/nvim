144826794c2224241953fe62894e3b19dd46f123
kedom <kedom@vmcall.net>
2026-06-14 20:20:35 +0200

    refactor(nvim): simplify lsp configuration

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,37 +1,11 @@
-{lib, ...}: {
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>i";
-      action = lib.nixvim.mkRaw ''
-        vim.diagnostic.open_float
-      '';
-      options.desc = "Show diagnostics under cursor";
-    }
-    {
-      mode = "n";
-      key = "<leader>rl";
-      action = lib.nixvim.mkRaw ''
-        function()
-          vim.cmd("lsp restart")
-          vim.cmd("bufdo e")
-        end
-      '';
-      options.desc = "Restart LSP and reload all buffers";
-    }
-  ];
-
+{
   plugins.lspconfig.enable = true;
   lsp.servers = {
     nixd.enable = true;
     jsonls.enable = true;
-    yamlls.enable = true;
-    tailwindcss.enable = true;
     dockerls.enable = true;
-    helm_ls.enable = true;
     tofu_ls.enable = true;
     rust_analyzer.enable = true;
-    astro.enable = true;
     vtsls = {
       enable = true;
       config.settings = {
