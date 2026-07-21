2cc95c83f89589ee2b03192ea05d6375ae9b312f
kedom <kedom@vmcall.net>
2026-07-21 14:15:37 +0200

    feat: update and switch to tsgo

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -7,12 +7,13 @@
     dockerls.enable = true;
     tofu_ls.enable = true;
     rust_analyzer.enable = true;
-    vtsls = {
-      enable = true;
-      config.settings = {
-        typescript.tsserver.maxTsServerMemory = 12288;
-        vtsls.autoUseWorkspaceTsdk = true;
-      };
-    };
+    tsgo.enable = true;
+    # vtsls = {
+    #   enable = true;
+    #   config.settings = {
+    #     typescript.tsserver.maxTsServerMemory = 12288;
+    #     vtsls.autoUseWorkspaceTsdk = true;
+    #   };
+    # };
   };
 }
