1ab823bacf20737077a0189521b117aaffc5c146
kedom <kedom@vmcall.net>
2026-07-27 13:00:26 +0200

    chore(lsp): remove commented-out vtsls configuration

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -8,12 +8,5 @@
     tofu_ls.enable = true;
     rust_analyzer.enable = true;
     tsgo.enable = true;
-    # vtsls = {
-    #   enable = true;
-    #   config.settings = {
-    #     typescript.tsserver.maxTsServerMemory = 12288;
-    #     vtsls.autoUseWorkspaceTsdk = true;
-    #   };
-    # };
   };
 }
